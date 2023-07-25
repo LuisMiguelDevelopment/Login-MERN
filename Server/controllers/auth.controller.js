6ea98cd585fdc9fe238c349b393fa6cc3b896d56
luismiguelalvarezmarin109 <luismiguelalvarezmarin109@gmail.com>
2023-07-25 17:20:22 -0500

    Jwt , bcrypt ,cookie terminado

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -1,17 +1,26 @@
 import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+import { createToeknAccess } from '../libs/jwt.js';
 
 
 export const register = async (req , res)=>{
     const {email,password,username}= req.body;
 
     try {
+        const passwordHash = await bcrypt.hash(password, 10)
         const newUser = new User({
            username,
            email,
-           password 
+           password:passwordHash 
         });
         const userSaved = await newUser.save();
-        return res.status(201).json(userSaved);
+        const token = await createToeknAccess({id:userSaved._id});
+        res.cookie('token',token)
+        res.status(201).json({
+            id:userSaved._id,
+            username:userSaved.username,
+            email :userSaved.email
+        });
     } catch (error) {
         res.status(500).json({message:error.message})
     }
