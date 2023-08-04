324eab5a95626b696f3e46ca4c44f78f82084030
Luis Miguel <luismiguelalvarezmarin109@gmail.com>
2023-08-04 08:30:03 -0500

    Register por terminar

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,9 +1,19 @@
 
 
+
+
 function RegisterPage() {
+
+    
+
   return (
     <div>
-        Register Page
+        <form action="">
+            <input type="text" name="" id="" />
+            <input type="email" name="" id="" />
+            <input type="password" name="" id="" />
+            <button type="submit" className="rounded-full">Register</button>
+        </form>
     </div>
   )
 }
